3b9897b2ab04a80f5e610a00fb18d3c658d4c68d
Tanguy Westelynck <tanguy.westelynck@evaldepol.com>
2020-04-28 10:45:49 +0200

    Correction des chemins relatifs pour les pages d'erreur et ajout d'icônes manquantes

--- www/controllers/error.php
+++ www/controllers/error.php
@@ -7,10 +7,14 @@ $menu = "error";
 $error = "404";
 $message = "Une erreur c'est produite, cette page introuvable !";
 if (!empty($_GET) && isset($_GET['e'])) {
+    if ($_GET['e'] === "403") {
+        $error = "403";
+    }
     if ($_GET['e'] === "500") {
         $error = "500";
         $message = "Le serveur rencontre un problème, veuillez nous excuser";
     }
 }
 $template = RELATIVE_PATH['views'] . $menu;
+
 include RELATIVE_PATH['views'] . "layout.phtml";
--- www/headers/headers.php
+++ www/headers/headers.php
@@ -3,8 +3,7 @@
 /* Definition des chemins relatif et absolu */
 define("ABSOLUTE_PATH", substr(__DIR__, 0, -7));
 define("RELATIVE_PATH", [
-    "controllers" => "http://localhost/sites/tanguygames/www/" . "controllers/",
-    //"controllers" => ABSOLUTE_PATH . "controllers/",
+    "controllers" => ABSOLUTE_PATH . "controllers/",
     "models" => ABSOLUTE_PATH . "models/",
     "classes" => ABSOLUTE_PATH . "classes/",
     "views" => ABSOLUTE_PATH . "views/",
@@ -12,8 +11,6 @@ define("RELATIVE_PATH", [
     "views_boards" => ABSOLUTE_PATH . "views/boards/",
     "headers" => ABSOLUTE_PATH . "headers/",
     "database" => ABSOLUTE_PATH . "database/",
-    //"root" => ABSOLUTE_PATH,
-    "root" => "http://localhost/sites/tanguygames/www/",
 ]);
 
 /* Constantes*/
@@ -35,7 +32,7 @@ include RELATIVE_PATH['classes'] . "Session.php";
 /* Fonction pour charger une icône en svg */
 function icon($name, $dimensions)
 {
-    return '<svg class="icons i_' . $dimensions . '" role="img"><use xlink:href="' . RELATIVE_PATH['root'] . 'css/icons/fonts-defs.svg#' . $name . '"></use></svg>';
+    return '<svg class="icons i_' . $dimensions . '" role="img"><use xlink:href="css/icons/fonts-defs.svg#' . $name . '"></use></svg>';
 }
 
 /* Fonction pour convertir une datetime en date fr */
